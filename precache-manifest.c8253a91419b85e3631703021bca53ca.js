4b1343341aaf596aef5d95368e60b7e4eea39279
Ben Lyaunzon <lyaunzon.b@gmail.com>
2020-07-24 13:18:08 -0700

    Updates

---
+++ precache-manifest.c8253a91419b85e3631703021bca53ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c5b08a794d8069c5be6717261a3ab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4fc544a80ae835aae44",
+    "url": "/static/css/main.e059a5ae.chunk.css"
+  },
+  {
+    "revision": "5db405c1c50bbaa2ce8e",
+    "url": "/static/js/2.84cc0fc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84cc0fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fc544a80ae835aae44",
+    "url": "/static/js/main.c7faab83.chunk.js"
+  },
+  {
+    "revision": "92a54ae2c259943b800c",
+    "url": "/static/js/runtime-main.f6227492.js"
+  },
+  {
+    "revision": "cc9c1a6656a0c57c9266eb6aa8de724f",
+    "url": "/static/media/GoodGlyphs-No1.cc9c1a66.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/speykani.us/precache-manifest.642a1cac76f58958e1db9d1d0d8cff55.js"
+  "/precache-manifest.c8253a91419b85e3631703021bca53ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/speykani.us/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
