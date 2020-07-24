bf88b2337989ab343abf37aa1b3640d487b0e938
Ben Lyaunzon <lyaunzon.b@gmail.com>
2020-07-24 04:48:27 -0700

    Updates

---
+++ precache-manifest.a733ff4c32989b388a9bf0c224464cb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea0009559dcb2fe79966852db13d704",
+    "url": "/speykani.us/index.html"
+  },
+  {
+    "revision": "fcf479c1691762320672",
+    "url": "/speykani.us/static/css/main.2416553f.chunk.css"
+  },
+  {
+    "revision": "5db405c1c50bbaa2ce8e",
+    "url": "/speykani.us/static/js/2.84cc0fc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speykani.us/static/js/2.84cc0fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf479c1691762320672",
+    "url": "/speykani.us/static/js/main.051c6e26.chunk.js"
+  },
+  {
+    "revision": "a6455b10429567aacda3",
+    "url": "/speykani.us/static/js/runtime-main.f2714065.js"
+  },
+  {
+    "revision": "cc9c1a6656a0c57c9266eb6aa8de724f",
+    "url": "/speykani.us/static/media/GoodGlyphs-No1.cc9c1a66.ttf"
+  }
+]);
